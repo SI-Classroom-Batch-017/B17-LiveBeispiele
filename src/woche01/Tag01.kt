53a3c0fe62e6481aa9adf2d3854db398def2fcd9
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-03-25 12:16:15 +0000

    Tag01: Hello World:
    Mehr Beispiele dazu,
    mehr Kommentare dazu,
    Compilerfehlerbeispiel auskommentiert dazu

--- src/woche01/Tag01.kt
+++ src/woche01/Tag01.kt
@@ -1,10 +1,30 @@
 // package = Ordner. Diese Datei befindet sich im Ordner "woche01"
 package woche01
 
-// die main() Funktion ist der Einstiegspunkt eines Kotlin-Programms.
-// Wird aufgerufen, wenn ein Programm gestartet wird
+/*
+ die main() Funktion ist der Einstiegspunkt eines Kotlin-Programms.
+ Wird aufgerufen, wenn ein Programm gestartet wird
+ */
+
+
 fun main() {
     // println druckt (= print) eine Zeichenfolge auf der Standardausgabe (Konsole) aus, gefolgt von einem Zeilenumbruch (=ln, line)
+    // LiveBeispiel 1.1
+    print("Nach dieser Zeile kommt kein Zeilenumbruch...") // print ohne ln macht keinen Zeilenumbruch
+    println("Hello World! 1")
+    println() // leere Zeile wird gedruckt
+    // println("Hello World! 2") // auskommentierter Code, wird nicht ausgeführt
+    println()
+    println("Hello World! 3")
+    println("Hello World! 4")
 
-    println("Hello World!")
+    // Compilefehler:
+    // unbekannte Funktion:
+    //prnnnnln("Hallo")
+    // unbekannte Syntax:
+    //bla bla hallo wie geht's
+
+    // geschweifte Klammer schreiben: option + 8 { }
+
+
 }
