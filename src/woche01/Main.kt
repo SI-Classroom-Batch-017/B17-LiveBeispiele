1add78673ce445497c7b9826ef2235ed82671437
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-03-25 06:54:01 +0000

    Tag01: Hello World

--- src/woche01/Main.kt
+++
@@ -1,5 +0,0 @@
-package woche01
-
-fun main() {
-    println("Hello World!")
-}
---
+++ src/woche01/Tag01.kt
@@ -0,0 +1,9 @@
+// package = Ordner. Diese Datei befindet sich im Ordner "woche01"
+package woche01
+
+// die main() Funktion ist der Einstiegspunkt eines Kotlin-Programms.
+// Wird aufgerufen, wenn ein Programm gestartet wird
+fun main() {
+    // println druckt (= print) eine Zeichenfolge auf der Standardausgabe (Konsole) aus, gefolgt von einem Zeilenumbruch (=ln, line)
+    println("Hello World!")
+}
