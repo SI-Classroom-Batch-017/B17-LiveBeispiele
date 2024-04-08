1d38b0de1a58ff4240316430447a069c895b2a93
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-04-08 09:48:25 +0100

    Tag06: if else, weitere beispiele

--- src/woche02/Tag01Verzweigungen/WhenBeispiele.kt
+++ src/woche02/Tag01Verzweigungen/WhenBeispiele.kt
@@ -20,8 +20,42 @@ fun main() {
         else -> println("Ungueltige Eingabe...")
     }
 
+
+    println("Bitte gib eine Zahl von 1-12 ein und wir sagen dir, zu welcher Jahreszeit der entsrpechende Monat gehört!")
+
+    var monatsInput: String = readln()
+
+
+    when(monatsInput){
+        "1","2","12" -> {
+            println("Winter")
+            when(monatsInput){
+                "1" -> println("Januar")
+                "2" -> println("Februar")
+                "12" -> println("Dezember")
+            }
+        }
+        "3","4","5" -> {
+            println("Frühling")
+            if(monatsInput == "3"){
+                println("März")
+            } else if(monatsInput == "4"){
+                println("April")
+            } else {
+                println("Mai")
+            }
+
+        }
+        "6","7","8" -> println("Sommer")
+        "9","10","11" -> println("Herbst")
+    }
+
+// Alterspruefung
+
+
+
+
 }
 
 
 
-// Alterspruefung
