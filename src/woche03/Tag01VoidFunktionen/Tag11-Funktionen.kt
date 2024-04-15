2509c1291f6b5a36142485e33b8ab5c06c5b58df
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-04-15 09:48:05 +0100

    W3: Funktionen Einstieg printName ausgelagert

--- src/woche03/Tag01VoidFunktionen/Tag11-Funktionen.kt
+++ src/woche03/Tag01VoidFunktionen/Tag11-Funktionen.kt
@@ -16,19 +16,7 @@ fun main() {
 }
 
 // Sinn von Funktionen: Wiederholbarer Code, der nicht immmer wieder geschrieben werden muss!
-fun printName(){
-    println("Wie ist dein Name?")
-    val name: String = readln()
-    println("Bitte Tageszeit angeben (morgens/mittags/abends")
-    var timeOfDay: String = readln()
-    if (timeOfDay == "morgens"){
-        println("Guten Morgen, $name!")
-    } else if (timeOfDay == "mittags"){
-        println("Guten Appetit in der Mittagspause, $name!")
-    } else if (timeOfDay == "abends"){
-        println("Guten Abend, $name")
-    }
-}
+
 
 // ## Aufgabe 6: Sternchen drucken
 //Erstelle eine Funktion `printStars()`, die mit Hilfe der `repeat()`-Funktion 13 Sternchen (*)
--- src/woche03/Tag01VoidFunktionen/printNameFunktion.kt
+++ src/woche03/Tag01VoidFunktionen/printNameFunktion.kt
@@ -1,2 +1,15 @@
 package woche03.Tag01VoidFunktionen
 
+fun printName(){
+    println("Wie ist dein Name?")
+    val name: String = readln()
+    println("Bitte Tageszeit angeben (morgens/mittags/abends")
+    var timeOfDay: String = readln()
+    if (timeOfDay == "morgens"){
+        println("Guten Morgen, $name!")
+    } else if (timeOfDay == "mittags"){
+        println("Guten Appetit in der Mittagspause, $name!")
+    } else if (timeOfDay == "abends"){
+        println("Guten Abend, $name")
+    }
+}
