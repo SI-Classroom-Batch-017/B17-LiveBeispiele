1535ba91e5ead907ff34d50e613694efd9187e0b
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-04-19 09:24:12 +0100

    Game (Aufgabe 3 gestern): fertige loesung

--- src/woche03/Game.kt
+++ src/woche03/Game.kt
@@ -1,7 +1,5 @@
 package woche03
 
-import kotlin.random.Random
-
 var spieler1Name = ""
 var spieler2Name = ""
 
@@ -13,7 +11,6 @@ var heilen: Int = 0
 var schaden: Int = 0
 
 fun main() {
-
     // -------------------------------------------------------------
     //
     //                      Das Duell
@@ -36,7 +33,18 @@ fun main() {
     runde()
     runde()
 
+    spielEnde()
+}
+
+fun spielEnde() {
     println("Spiel zu Ende")
+    if (spieler1Leben > spieler2Leben) {
+        println("$spieler1Name hat gewonnen!")
+    } else if (spieler2Leben > spieler1Leben) {
+        println("$spieler2Name hat gewonnen!")
+    } else {
+        println("Unentschieden!")
+    }
 }
 
 fun spielStart() {
@@ -51,8 +59,7 @@ fun spielStart() {
     spieler1Leben = (500..600).random()
     spieler2Leben = (500..600).random()
 
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
+    printLifePoints()
     println()
 
     println("Es findet ein Duell zwischen $spieler1Name und $spieler2Name statt.")
@@ -62,42 +69,111 @@ fun spielStart() {
 
 fun runde() {
     //Spielrunde
+    // 1. Spieler1 und Spieler2 greifen sich nacheinander an
+    attackPhase()
+
+    // beide Spieler heilen sich:
+    healPhase()
+
+    // Spieler1 und Spieler2 greifen sich nacheinander mit Spezialattacke an
+    specialAttackPhase()
+}
+
+
+fun specialAttackPhase() {
+    spieler2Leben -= specialAttack(spieler1Name, spieler2Name)
+    printLifePoints()
+
+    spieler1Leben -= specialAttack(spieler2Name, spieler1Name)
+    printLifePoints()
+}
+
+fun specialAttack(angreifer: String, opfer: String): Int {
+    printSpecial()
+    val auswahl = printSpecialAttack(angreifer)
+    return specialAttackPicker(auswahl, angreifer, opfer)
+}
+
+fun specialAttackPicker(auswahl: Int, angreifer: String, opfer: String): Int {
+    when (auswahl) {
+        1 -> {
+            val schaden = 25
+            println("$angreifer fügt $opfer mit der Spezialattacke Schwertschlag $schaden zu.")
+            return schaden
+        }
+
+        2 -> {
+            val schaden = 50
+            println("$angreifer fügt $opfer mit der Spezialattacke Feuerball $schaden zu.")
+            return schaden
+        }
+
+        3 -> {
+            val schaden = 30
+            println("$angreifer fügt $opfer mit der Spezialattacke Giftangriff $schaden zu.")
+            return schaden
+        }
+
+        else -> {
+            println("Ungültige Auswahl. Normale Attacke wird durchgeführt.")
+            val schaden = (10..100).random()
+            println("$angreifer greift $opfer an und verursacht $schaden Schadenspunkte.")
+            return schaden
+        }
+    }
+}
+
+fun printSpecialAttack(angreifer: String): Int {
+    println("Welche Spezialattacke soll $angreifer ausführen?")
+    println("Diese Spezialattacken sind verfügbar:")
+    println("1. Schwertschlag")
+    println("2. Feuerball")
+    println("3. Giftangriff")
+    println("Bitte gib den Index für die Spezialattacke an.")
+
+    val auswahl = readln().toInt()
+    return auswahl
+}
+
+fun printSpecial() {
     println(
-        "     ___   .___________.___________.    ___       ______  __  ___ \n" +
-                "    /   \\  |           |           |   /   \\     /      ||  |/  / \n" +
-                "   /  ^  \\ `---|  |----`---|  |----`  /  ^  \\   |  ,----'|  '  /  \n" +
-                "  /  /_\\  \\    |  |        |  |      /  /_\\  \\  |  |     |    <   \n" +
-                " /  _____  \\   |  |        |  |     /  _____  \\ |  `----.|  .  \\  \n" +
-                "/__/     \\__\\  |__|        |__|    /__/     \\__\\ \\______||__|\\__\\ \n" +
+        "     _______..______    _______  ________   __       ___       __      \n" +
+                "    /       ||   _  \\  |   ____||       /  |  |     /   \\     |  |     \n" +
+                "   |   (----`|  |_)  | |  |__   `---/  /   |  |    /  ^  \\    |  |     \n" +
+                "    \\   \\    |   ___/  |   __|     /  /    |  |   /  /_\\  \\   |  |     \n" +
+                ".----)   |   |  |      |  |____   /  /----.|  |  /  _____  \\  |  `----.\n" +
+                "|_______/    | _|      |_______| /________||__| /__/     \\__\\ |_______|\n" +
                 "\n"
     )
+}
 
-    schaden = (10..100).random()
-    println("$spieler1Name greift $spieler2Name an und verursacht $schaden Schadenspunkte.")
-    spieler2Leben -= schaden
+fun attackPhase() {
+    // attacke gibt uns den Schaden zurueck, der dem Opfer abgezogen werden soll.
+    // 1. Spieler1 greift Spieler2 an
+    spieler2Leben = spieler2Leben - attack(spieler1Name, spieler2Name)
+    printLifePoints()
+
+    // 1. Spieler2 greift Spieler1 an
+    // kurze schreibweise zum - rechnen:
+    spieler1Leben -= attack(spieler2Name, spieler1Name)
+    printLifePoints()
+}
+
+
+fun healPhase() {
+    printHeal()
+
+    spieler1Leben += healPlayer(spieler1Name)
+    spieler2Leben += healPlayer(spieler2Name)
+
     println()
+    printLifePoints()
+}
 
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
 
-    println(
-        "     ___   .___________.___________.    ___       ______  __  ___ \n" +
-                "    /   \\  |           |           |   /   \\     /      ||  |/  / \n" +
-                "   /  ^  \\ `---|  |----`---|  |----`  /  ^  \\   |  ,----'|  '  /  \n" +
-                "  /  /_\\  \\    |  |        |  |      /  /_\\  \\  |  |     |    <   \n" +
-                " /  _____  \\   |  |        |  |     /  _____  \\ |  `----.|  .  \\  \n" +
-                "/__/     \\__\\  |__|        |__|    /__/     \\__\\ \\______||__|\\__\\ \n" +
-                "\n"
-    )
 
-    schaden = (10..100).random()
-    println("$spieler2Name greift $spieler1Name an und verursacht $schaden Schadenspunkte.")
-    spieler1Leben -= schaden
-    println()
 
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
-
+fun printHeal() {
     println(
         " __    __   _______  __   __       _______ .__   __. \n" +
                 "|  |  |  | |   ____||  | |  |     |   ____||  \\ |  | \n" +
@@ -107,115 +183,48 @@ fun runde() {
                 "|__|  |__| |_______||__| |_______||_______||__| \\__| \n" +
                 "\n"
     )
+}
 
+fun healPlayer(zuHeilenderPlayer: String): Int {
     heilen = (5..50).random()
-    println("$spieler1Name heilt sich um $heilen Lebenspunkte.")
-    spieler1Leben += heilen
+    println("$zuHeilenderPlayer heilt sich um $heilen Lebenspunkte.")
+    return heilen
+}
 
-    heilen = (5..50).random()
-    println("$spieler2Name heilt sich um $heilen Lebenspunkte.")
-    spieler2Leben += heilen
+
+
+
+
+fun attack(angreifer: String, opfer: String): Int {
+    printAttack()
+
+    schaden = (10..100).random()
+    println("$angreifer greift $opfer an und verursacht $schaden Schadenspunkte.")
+    return schaden
+    // alternative ohne return, einfach auf die globalen Variablen zugreifen darauf ankommend, wer gerade angreift/opfer ist:
+    if (angreifer == spieler1Name) {
+        spieler2Leben -= schaden
+    }
+    else {
+        spieler1Leben -= schaden
+    }
+}
+
+fun printAttack() {
+    println(
+        "     ___   .___________.___________.    ___       ______  __  ___ \n" +
+                "    /   \\  |           |           |   /   \\     /      ||  |/  / \n" +
+                "   /  ^  \\ `---|  |----`---|  |----`  /  ^  \\   |  ,----'|  '  /  \n" +
+                "  /  /_\\  \\    |  |        |  |      /  /_\\  \\  |  |     |    <   \n" +
+                " /  _____  \\   |  |        |  |     /  _____  \\ |  `----.|  .  \\  \n" +
+                "/__/     \\__\\  |__|        |__|    /__/     \\__\\ \\______||__|\\__\\ \n" +
+                "\n"
+    )
+}
+
+
+fun printLifePoints() {
+    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
+    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
     println()
-
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
-
-    println(
-        "     _______..______    _______  ________   __       ___       __      \n" +
-                "    /       ||   _  \\  |   ____||       /  |  |     /   \\     |  |     \n" +
-                "   |   (----`|  |_)  | |  |__   `---/  /   |  |    /  ^  \\    |  |     \n" +
-                "    \\   \\    |   ___/  |   __|     /  /    |  |   /  /_\\  \\   |  |     \n" +
-                ".----)   |   |  |      |  |____   /  /----.|  |  /  _____  \\  |  `----.\n" +
-                "|_______/    | _|      |_______| /________||__| /__/     \\__\\ |_______|\n" +
-                "\n"
-    )
-
-    println("Welche Spezialattacke soll $spieler1Name ausführen?")
-    println("Diese Spezialattacken sind verfügbar:")
-    println("1. Schwertschlag")
-    println("2. Feuerball")
-    println("3. Giftangriff")
-    println("Bitte gib den Index für die Spezialattacke an.")
-
-    val auswahl1Runde1 = readln().toInt()
-
-    when (auswahl1Runde1) {
-        1 -> {
-            val schaden = 25
-            println("$spieler1Name fügt $spieler2Name mit der Spezialattacke Schwertschlag $schaden zu.")
-            spieler2Leben -= schaden
-        }
-
-        2 -> {
-            val schaden = 50
-            println("$spieler1Name fügt $spieler2Name mit der Spezialattacke Feuerball $schaden zu.")
-            spieler2Leben -= schaden
-        }
-
-        3 -> {
-            val schaden = 30
-            println("$spieler1Name fügt $spieler2Name mit der Spezialattacke Giftangriff $schaden zu.")
-            spieler2Leben -= schaden
-        }
-
-        else -> {
-            println("Ungültige Auswahl. Normale Attacke wird durchgeführt.")
-            schaden = (10..100).random()
-            println("$spieler1Name greift $spieler2Name an und verursacht $schaden Schadenspunkte.")
-            spieler2Leben -= schaden
-        }
-    }
-
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
-
-    println(
-        "     _______..______    _______  ________   __       ___       __      \n" +
-                "    /       ||   _  \\  |   ____||       /  |  |     /   \\     |  |     \n" +
-                "   |   (----`|  |_)  | |  |__   `---/  /   |  |    /  ^  \\    |  |     \n" +
-                "    \\   \\    |   ___/  |   __|     /  /    |  |   /  /_\\  \\   |  |     \n" +
-                ".----)   |   |  |      |  |____   /  /----.|  |  /  _____  \\  |  `----.\n" +
-                "|_______/    | _|      |_______| /________||__| /__/     \\__\\ |_______|\n" +
-                "\n"
-    )
-
-    println("Welche Spezialattacke soll $spieler2Name ausführen?")
-    println("Diese Spezialattacken sind verfügbar:")
-    println("1. Schwertschlag")
-    println("2. Feuerball")
-    println("3. Giftangriff")
-    println("Bitte gib den Index für die Spezialattacke an.")
-
-    val auswahl2Runde1 = readln().toInt()
-
-    when (auswahl2Runde1) {
-        1 -> {
-            val schaden = 25
-            println("$spieler2Name fügt $spieler1Name mit der Spezialattacke Schwertschlag $schaden zu.")
-            spieler1Leben -= schaden
-        }
-
-        2 -> {
-            val schaden = 50
-            println("$spieler2Name fügt $spieler1Name mit der Spezialattacke Feuerball $schaden zu.")
-            spieler1Leben -= schaden
-        }
-
-
-        3 -> {
-            val schaden = 30
-            println("$spieler2Name fügt $spieler1Name mit der Spezialattacke Giftangriff $schaden zu.")
-            spieler1Leben -= schaden
-        }
-
-        else -> {
-            println("Ungültige Auswahl. Normale Attacke wird durchgeführt.")
-            schaden = (10..100).random()
-            println("$spieler2Name greift $spieler1Name an und verursacht $schaden Schadenspunkte.")
-            spieler1Leben -= schaden
-        }
-    }
-
-    println("$spieler1Name hat noch $spieler1Leben Lebenspunkte.")
-    println("$spieler2Name hat noch $spieler2Leben Lebenspunkte.")
 }
