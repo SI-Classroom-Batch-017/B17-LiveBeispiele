57c1828779944216bb516d19bd80e6065ab7a1bc
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-05-06 09:35:20 +0100

    Computational Thinking: Worte zählen (mit und ohne Sonderzeichen)

--- src/woche05/Tag05/Tag25-BuchstabenZaehlen.kt
+++ src/woche05/Tag05/Tag25-BuchstabenZaehlen.kt
@@ -26,22 +26,76 @@ fun main() {
             "Let's go to perfect places \n".lowercase()
 
 
-    // vorkommen des buchstabens 'a' zählen
+    // TODO vorkommen des buchstabens 'a' zählen
+
+//    val aVorkommen: Int = findChar(lyrics, 'a')
+//    val bVorkommen: Int = findChar(lyrics, 'b')
 
 
+    // vorkommen aller buchstaben zählen
 
-    // TODO vorkommen aller buchstaben zählen
+    //countAllLetters(lyrics)
+
+    // alle Zeichen im String zählen:
+    var allChars: Int = lyrics.length
+
 
     // TODO Anzahl aller Buchstaben ohne Leerzeichen, ohne Sonderzeichen
 
-    // TODO alle worte zählen
+        // 1. Ansatz mit split: gibt uns liste an worten zurueck
+        var allLetters: List<String> = lyrics.split(" ","'",",")
+        // Länge jedes Wortes in dieser Liste an Worten zum Counter addieren
+        var counter = 0
+        for (word in allLetters){
+            counter += word.length
+        }
+        println("Alle Buchstaben ohne Sonderzeichen: $counter")
+
+        // 2. Ansatz: mit replace alle Sonderzeichen / Leerzeichen ersetzen mit "", dann länge zählen
+        var words = lyrics.replace(" ","").replace("'","").replace(",","")
+        println(words.length)
+
+
+
+    //println(allLetters)
+    var allLettersString: String = allLetters.toString()
+   // println(allLettersString)
+
 
     // TODO längstes wort im text finden
 
     // TODO mehrere längste worte im text finden, wenn welche gleich lang sind
 
 
+}
 
+fun countAllLetters(lyrics: String) {
+    var counter = 0
+
+    for (letter in 'a'..'z') {
+        counter = 0
+        for (char in lyrics) {
+            if (letter == char) {
+                counter++
+            }
+        }
+        println("Vorkommen Buchstabe $letter: $counter")
+    }
+}
+
+fun findChar(lyrics: String, searchedChar: Char): Int {
+    var counter: Int = 0
+
+    for (char in lyrics) {
+        if (char == searchedChar) {
+            counter++
+        }
+    }
+    println("Der Buchstabe $searchedChar kommt $counter x vor.")
+
+    return counter
+
+    return lyrics.count { it == searchedChar }
 
 }
 
