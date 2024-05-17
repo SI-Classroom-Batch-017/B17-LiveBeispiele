7c3e9992e764b238e176af8a12bb228950ad0909
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2024-05-17 08:49:14 +0100

    W7T5: Beispiel Klasse, Konstruktor mit Pokemon. Beispiel Beutel mit funktionen, Beispiel useBeutel funktion, beispiel attacke

---
+++ src/woche07/Tag02KlasseMitAttacke/Beutel.kt
@@ -0,0 +1,47 @@
+package woche07.Tag02KlasseMitAttacke
+
+class Beutel {
+
+
+    //TODO unsere unterschiedlichen Tränke darstellen
+        // als map?
+        // als liste?
+        // vllt einfach die anzahl jedes tranks speichern?
+
+    var anzahlHeiltraenke: Int = 4
+    var anzahlBooster: Int = 2
+
+
+    //TODO Funktion pro Trank, um ihn zu benutzen
+
+    fun useHeiltrank(pokemon: Pokemon) {
+        // printlns, die bescheid sagen, was passiert
+        // hp um 50% der gesamthp heilen
+        if (anzahlHeiltraenke > 0 ) {
+            pokemon.hp += (pokemon.ursprungsHP / 2)
+            // anzahl der heiltraenke im beutel um 1 verringern
+            anzahlHeiltraenke--
+        } // else bescheid sagen, dass traenke leer sind
+    }
+
+    // alle angriffe sind ab jetzt 10% staerker
+    fun useBooster(held: Held) {
+        // printlns, die bescheid sagen, was passiert
+        if (anzahlBooster > 0) {
+            held.attackFaktor *= 1.1
+            // anzahl der heiltraenke im beutel um 1 verringern
+            anzahlBooster--
+        } // else bescheid sagen, dass booster leer sind
+    }
+
+}
+
+class Held(var name: String, var hp: Double){
+    var attackFaktor: Double = 1.0
+
+
+    fun attacke(gegner: Pokemon){
+        gegner.hp -= 45*attackFaktor
+    }
+
+}
---
+++ src/woche07/Tag02KlasseMitAttacke/utils.kt
@@ -0,0 +1,16 @@
+package woche07.Tag02KlasseMitAttacke
+
+import kotlin.math.roundToInt
+
+fun roundDouble(hp: Double): Double {
+    // 33.39475657483
+    var roundedHPToInt: Int = (hp*100).roundToInt() // 3339
+    var roundedHP: Double = roundedHPToInt/100.0 // 33.39
+    return roundedHP
+    // kurze version
+    return ((hp*100).roundToInt())/100.0
+}
+
+fun roundDoubleWithStringFormat(nr: Double): Double{
+    return String.format("%.2f",nr).replace(",",".").toDouble()
+}
